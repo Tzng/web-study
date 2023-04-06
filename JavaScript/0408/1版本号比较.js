5791bd33b871bc7eb15e40f2ef5ea41808446b01
allahbin <569693942@qq.com>
2023-04-06 15:21:43 +0800

    补充对应学习的js文件

---
+++ JavaScript/0408/1版本号比较.js
@@ -0,0 +1,14 @@
+// 传入两个版本号，打印出最大的那个版本号
+// 例如：传入 1.0.0 和 1.0.1，打印出 1.0.1
+// 例如：传入 1.0.0 和 1.0.0，打印出 1.0.0
+
+function compareVersion(v1, v2) {
+    // 这里写代码
+    return v1
+}
+
+const v1 = '1.0.0'
+const v2 = '1.0.1'
+const v3 = '1.0.0'
+console.log(compareVersion(v1, v2))
+console.log(compareVersion(v1, v3))
---
+++ JavaScript/0408/2数组删除.js
@@ -0,0 +1,10 @@
+/**
+ * 传入一个数组，删除里面的偶数，然后打印出来
+ * > 示例1：[1,2,3,4,5,6] => [1,3,5]
+ */
+function deleteEvenNumber(arr) {
+    // 这里写代码
+    return arr
+}
+const arr = [1, 2, 3, 4, 5, 6];
+console.log(deleteEvenNumber(arr))
