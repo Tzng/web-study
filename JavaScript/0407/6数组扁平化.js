5dc4f276d68de9d96dc0287549829edb3511bb1d
xxy <2236515040@qq.com>
2023-04-16 23:45:44 +0800

    0408 3 9

--- JavaScript/0407/6数组扁平化.js
+++ JavaScript/0407/6数组扁平化.js
@@ -1,7 +1,16 @@
+/**
+ * reduce() 方法是 JavaScript 数组的一个内置方法，它接收一个函数作为累加器，数组中的每个值（从左到右）开始缩减，最终计算为一个值。
+ * concat() 方法用于连接两个或多个数组。concat() 方法不会更改现有数组，而是返回一个新数组，其中包含已连接数组的值。
+ *
+ * prev.concat() 是将前面的结果与当前元素连接起来，
+ * 而 reduce() 方法是将前面的结果与当前元素计算后返回一个值。
+ * @param x
+ * @returns {*}
+ */
 function f(x) {
-    return x.reduce((prev,cur)=>{
-        return prev.concat(Array.isArray(cur)?f(cur):cur);
-    },[]);
+    return x.reduce((prev,cur)=>{       // reduce() 方法用于迭代输入数组的每个元素并将其与输出数组连接起来。
+        return prev.concat(Array.isArray(cur)?f(cur):cur);          //prev.concat() 是将前面的结果与当前元素连接起来
+    },[]);                     //[] 是 reduce() 方法的初始值
 }
 const a = [[1,3,4,5],[2,3,4,5,6,[2,4,5,6]]];
 console.log(f(a));
--- JavaScript/0408/9二维数组.js
+++ JavaScript/0408/9二维数组.js
@@ -1,15 +1,60 @@
 const a = [[1,3,4],[[2,8]]];
 const b =[1,3,4,5, [3,4,9]];
+function N1(x) {
+    return x.reduce((prev,cur)=>{
+        return prev.concat(Array.isArray(cur)?N1(cur):cur);
+    },[]);
+}
 function Max(x) {
-    let max=x[0][0];
-    for(var i= 0; i<x.length;i++){
-        for(var j = 0;j<x[i].length;j++){
-            if (x[i][j]>max){
-                max=x[i][j];
+    let max=x[0];
+    for(let i= 0; i<x.length; i++){
+            if (x[i]>max){
+                max=x[i];
             }
         }
-    }
     return max;
 }
-console.log(Max(a));
-console.log(Max(b));
+console.log(Max(N1(a)));//8
+
+console.log(Max(N1(b)));//9
+
+
+//AI
+function AImax(x) {
+    const max = x.reduce((c,d) => Math.max(c,Array.isArray(d)?Math.max(...d):d),-Infinity);
+    return max
+}
+console.log(AImax(a));//NaN
+console.log(AImax(b));//9
+
+
+//AI优化版
+function AImaxPro(x) {
+    const max = x.reduce((c,d) => {
+        if (Array.isArray(d)) {
+            return Math.max(c, Math.max(...d));
+        } else if (typeof d === 'number') {
+            return Math.max(c, d);
+        } else {
+            return c;
+        }
+    }, -Infinity);
+    return max;
+}
+console.log(AImaxPro(a));//NaN
+console.log(AImaxPro(b));//9
+
+
+function AImaxPro2(x) {
+    const max = x.reduce((c,d) => {
+        if (Array.isArray(d)) {
+            return Math.max(c, Math.max(...d));
+        } else if (typeof d === 'number' && !Number.isNaN(d)) {
+            return Math.max(c, d);
+        } else {
+            return c;
+        }
+    }, -Infinity);
+    return max;
+}
+console.log(AImaxPro2(a));//NaN
