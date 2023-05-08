eebfaafe41a876cbcb15ca0e6ff4b69b5d791e8e
allahbin <16008@theling.cn>
2023-05-09 00:06:40 +0800

    19题

--- Vue/src/service/CommService.ts
+++ Vue/src/service/CommService.ts
@@ -2,13 +2,63 @@ const CommService = {
     /**
      * 地区列表
      */
-    areaList: async () => {
+    areaList: async (pcode?: string) => {
+        const params = new FormData();
+        if(pcode){
+            params.append('pcode', pcode)
+        }
         const res = await fetch('https://api.jyfwyun.com/cloud-service/cross/regionList', {
             method:'POST',
+            body: new URLSearchParams(params).toString(),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
         });
         const json = await  res.json();
         return json.result;
     },
+    /**
+     * 获取主题数据
+     */
+    themeList: async () => {
+        const res = await fetch("https://api.jyfwyun.com/cloud-service/cross/themeList?entType=2&ac=120000");
+        const result = await res.json();
+        return result;
+    },
+    /**
+     * 搜索
+     */
+    search: async (params: any) => {
+        const res = await fetch("https://api.jyfwyun.com/cloud-service/cross/search", {
+            method:'POST',
+            body: JSON.stringify(params),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const json = await  res.json();
+        return json.result;
+    }
 }
 
 export default CommService;
+
+const themeList2 = async () => {
+    // 从A那拿到数据
+    const res = await fetch("https://api.jyfwyun.com/cloud-service/cross/themeList");
+    // 交给B处理
+    const result = await res.json();
+    // 交给C处理
+    const result2 = await delay(result);
+    // 交给D处理
+    const result3 = await delay(result2);
+}
+
+// 延迟两秒返数据
+const delay = (data) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(data);
+        }, 2000);
+    })
+}
--- Vue/src/views/S0419Fetch.vue
+++ Vue/src/views/S0419Fetch.vue
@@ -3,13 +3,29 @@
         <h1>0419-网络基础</h1>
         <h3>1. 获取地区数据{{areaList.length}}个</h3>
         <div style="height: 100px; overflow-y: auto">
-            <div v-for="item in areaList" :key="item.id">
+            <div v-for="item in areaList" :key="item.id" @click="showSubAreaList(item.id)">
                 {{item.text}}
             </div>
         </div>
-        <h3>2. 获取主题数据0个</h3>
+        <h3>3. 点击地区的子地区有{{subAreaList.length}}个</h3>
         <div style="height: 100px; overflow-y: auto">
-
+            <div v-for="item in subAreaList" :key="item.id">
+                {{item.text}}
+            </div>
+        </div>
+        <h3>2. 获取主题数据{{themeList.length}}个</h3>
+        <div style="height: 100px; overflow-y: auto">
+            <div v-for="item in themeList" :key="item.id">
+                {{item.name}}
+            </div>
+        </div>
+        <h3>4. 获取条目数据{{itemRecords.length}}个</h3>
+        <input v-model="keyword" >
+        <button @click="searchItem">搜索</button>
+        <div style="height: 100px; overflow-y: auto">
+            <div v-for="item in itemRecords" :key="item.scopeId">
+                {{item.standardItem}}
+            </div>
         </div>
     </div>
 </template>
@@ -19,13 +35,50 @@ import {onMounted, ref} from "vue";
 import CommService from "@/service/CommService";
 
 const areaList = ref([]);
+// 子地区数据
+const subAreaList = ref([]);
+
+const themeList = ref([]);
+
+const itemRecords = ref([]);
+
+// 搜索的关键词
+const keyword = ref('');
 
 onMounted(() => {
     initData();
+    initTheme();
+    searchItem();
 })
 
 const initData = async () => {
     areaList.value = await CommService.areaList();
+}
+
+const initTheme = async () => {
+    const result = await CommService.themeList();
+    themeList.value = result.result;
+}
+
+const showSubAreaList =async (id) => {
+    const result = await CommService.areaList(id);
+    subAreaList.value = result;
+}
+
+const searchItem = async () => {
+    const result = await CommService.search({
+        "s": "1681786960136",
+        "searchType": "item",
+        "pageNum": 1,
+        "busiCombosObj": {},
+        "pageSize": 5,
+        "searchSchema": "nlp",
+        "ac": "210000",
+        "industryTypes": [],
+        keyword: keyword.value
+    });
+    console.log('searchItem', result);
+    itemRecords.value = result.data.records;
 }
 
 </script>
