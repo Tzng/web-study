bc0bd7e0b248b0a54f76bbb2c4dc37b546acff60
allahbin <16008@theling.cn>
2023-05-29 22:43:44 +0800

    新增自己状态的button

--- React/src/components/MyButton.tsx
+++ React/src/components/MyButton.tsx
@@ -1,4 +1,5 @@
 import styles from './MyButton.less'
+import {useState} from "react";
 
 const style1 = {
     width: 200,
@@ -9,11 +10,14 @@ function MyButton({text = "点击我", onClick123131}: {
     text?: string,
     onClick123131?: () => void
 }) {
+
+    const [count, setCount] = useState(0);
+
     return (
         <div>
             <h1>About</h1>
-            <button className="myButton" onClick={onClick123131}>I'm a button</button>
-            <button style={style1} className={styles.myButton2}>{text}</button>
+            <button className="myButton" onClick={onClick123131}>点击了{text}</button>
+            <button style={style1} className={styles.myButton2} onClick={() => setCount(count + 1)}>自己的状态{count}</button>
         </div>
     );
 }
